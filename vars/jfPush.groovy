08149f270f9c0063dd9fdcdf75c472649ef6d8bb
Robert-aug13 <83607960+Robert-aug13@users.noreply.github.com>
2023-11-28 01:03:53 +0530

    Update jfPush.groovy

--- vars/jfPush.groovy
+++ vars/jfPush.groovy
@@ -1,5 +1,4 @@
 def call(){
 
-  sh 'cd /var/lib/jenkins/workspace/demo1/target'
-  sh 'curl -X PUT -u admin:Pass@1234 -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://http://35.209.248.239:8082/artifactory/example-repo-local/'
+  sh 'curl -X PUT -u admin:Pass@1234 -T  /var/lib/jenkins/workspace/demo1/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://35.209.248.239:8082/artifactory/example-repo-local/'
 }
